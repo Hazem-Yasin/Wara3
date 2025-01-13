a70557de66a3c69bc2fdeeb7bbb9384653c2a89c
Hazem Yasin <hazem.yasin.1416@gmail.com>
2025-01-13 22:29:19 +0200

    removed all angular projects, will start new

--- Wara3_FullStack_Angular_.net/Wara3_FullStack_Angular_.net.AppHost/Program.cs
+++
@@ -1,5 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder.AddProject<Projects.Wara3_FullStack_Angular__net_Server>("wara3-fullstack-angular-net-server");
-
-builder.Build().Run();
